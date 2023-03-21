b49c1890fa6640ccb243da9cbce975448337b73b
yaracettaga@gmail.com <yaracettaga@gmail.com>
2023-03-21 17:11:22 -0600

    Actividad realizada en retroalimentación de código

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // Escribe tu código aquí.
+
+const myArray = ['Computadora', 'Pozole', 'tablet', 'perro', 'gato', 'celular'];
+
+function palabraGrande (str, arr){
+
+    return arr.filter((word) => word.lengh > str.lengh); 
+}
+
+console.log('tablet', palabraGrande);
+
+function impArray (arr ){
+
+    const ul = documet.createElement ('ul');
+    arr.((word) => {
+
+        const li = document.createElement('')
+    })
+}
